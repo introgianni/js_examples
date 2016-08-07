616865bac9969346b64bcf606436dae3258c489c
introgianni <gianni.introna@gmail.com>
2016-08-07 13:22:03 +0200

    modificati 2 indirizzi prova new folder

--- prova.js
+++ prova.js
@@ -2,13 +2,14 @@
 var o = {
         name = "Gianni",
         work = "about programming!",
-        address = "via kkklk",
+        address = "via roma",
         phone = 44444
+
     }
     //file vuoto con solo un oggetto
 var object2 = {
     name = "Mary",
     work = "teacher",
-    address = "via kkklk",
+    address = "via mazzini",
     phone = 87877
 }
