995bce5e78dd17e640e48e1f2f14de40dd9ca27d
introgianni <gianni.introna@gmail.com>
2016-08-07 11:33:28 +0200

    aggiunto nuobvo ogetto

--- prova.js
+++ prova.js
@@ -1,7 +1,14 @@
 //file vuoto con solo un oggetto
 var o = {
-    name = "Gianni",
-    work = "about programming!",
+        name = "Gianni",
+        work = "about programming!",
+        address = "via kkklk",
+        phone = 44444
+    }
+    //file vuoto con solo un oggetto
+var object2 = {
+    name = "Mary",
+    work = "teacher",
     address = "via kkklk",
     phone = 87877
 }
